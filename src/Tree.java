25cffdbcd787e2b4271a3f02d175bc2c23665092
Gosha Petrov <gosha_petrov20@rambler.ru>
2023-12-01 20:59:22 +0300

    First publication

---
+++ src/Tree.java
@@ -0,0 +1,90 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Tree {
+    private String name;
+    private Tree left;
+    private Tree right;
+
+    public Tree(String name) {
+        this.name = name;
+    }
+
+    public boolean contains(String query) {
+        if (query.compareTo(name) < 0) {
+            return left != null && left.contains(query);
+        } else if (query.compareTo(name) > 0) {
+            return right != null && right.contains(query);
+        } else {
+            return name.equals(query);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Tree getLeft() {
+        return left;
+    }
+
+    public void setLeft(Tree left) {
+        this.left = left;
+    }
+
+    public Tree getRight() {
+        return right;
+    }
+
+    public void setRight(Tree right) {
+        this.right = right;
+    }
+
+    public boolean isNamePyramid() {
+        if (left != null && right != null) {
+            if (name.length() > left.name.length() || name.length() > right.name.length()) {
+                return false;
+            }
+            return left.isNamePyramid() && right.isNamePyramid();
+        }
+        if (left != null) {
+            return (name.length() <= left.name.length()) ? left.isNamePyramid() : false;
+        }
+        if (right != null) {
+            return (name.length() <= right.name.length()) ? right.isNamePyramid() : false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String[] leftLines = (left != null ? left.toString() : "").split("\n");
+        String[] rightLines = (right != null ? right.toString() : "").split("\n");
+
+        int maxLeftSize = Arrays.stream(leftLines)
+                .map(String::length)
+                .max(Comparator.naturalOrder())
+                .orElse(0);
+
+        List<String> lines = new ArrayList<>();
+        lines.add(" ".repeat(maxLeftSize + 1) + name);
+        for (int i = 0; i < Math.max(leftLines.length, rightLines.length); i++) {
+            String prefix = i < leftLines.length ? leftLines[i] : "";
+            lines.add(
+                    prefix +
+                            " ".repeat(maxLeftSize + name.length() + 1 * 2 - prefix.length()) +
+                            (i < rightLines.length ? rightLines[i] : "")
+            );
+        }
+
+        return lines.stream().collect(Collectors.joining("\n"));
+    }
+}
